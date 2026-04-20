603aaa7d45ab467b99b0408684363f7d6df316f5
Utro <tomiriszazykbayeva@gmail.com>
2026-04-20 10:13:27 +0500

    adding zoneless

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,37 +1,20 @@
+// app.routes.ts
 import { Routes } from '@angular/router';
+import { LoginComponent } from './features/auth/login/login';
+import { RegisterComponent } from './features/auth/register/register';
+import { ProfileComponent } from './features/auth/profile/profile';
 import { authGuard } from './core/guards/auth.guard';
 
 export const routes: Routes = [
-  {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
-  },
-  {
-    path: 'login',
-    loadComponent: () =>
-      import('./features/auth/login/login').then(m => m.LoginComponent)
-  },
-  {
-    path: 'register',
-    loadComponent: () =>
-      import('./features/auth/register/register').then(m => m.RegisterComponent)
-  },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
   {
     path: 'profile',
-    loadComponent: () =>
-      import('./features/auth/profile/profile').then(m => m.ProfileComponent),
+    component: ProfileComponent,
     canActivate: [authGuard]
   },
 
-  // ── Команда добавляет свои маршруты ниже ──────────────────────────────────
-  // { path: 'home',      loadComponent: () => import('./features/items/home/home').then(m => m.Home) },
-  // { path: 'items/:id', loadComponent: () => import('./features/items/detail/item-detail').then(m => m.ItemDetail) },
-  // { path: 'claims',    loadComponent: () => import('./features/claims/claims').then(m => m.Claims), canActivate: [authGuard] },
-  // ──────────────────────────────────────────────────────────────────────────
-
-  {
-    path: '**',
-    redirectTo: 'login'
-  }
+  { path: '**', redirectTo: '/login' }
 ];
